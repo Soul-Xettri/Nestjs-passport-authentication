1c5792fce6498ed5c88e2343dc5df7b05a39f11d
sou-xettri <rakeshchhetri420@gmail.com>
2024-03-07 09:19:32 +0545

    Init: auth mvc module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
 })
 export class AppModule {}
