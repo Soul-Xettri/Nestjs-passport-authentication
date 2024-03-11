41af4cd44e5e68a46b853ee332dc6ad686a552e9
sou-xettri <rakeshchhetri420@gmail.com>
2024-03-11 10:46:57 +0545

    Delete: unused files and update module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { AtGuard, UserCheckGuard } from './common/guards';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
-  imports: [PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // no need to import into other modules
+    }),
+    PrismaModule,
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: UserCheckGuard,
+    },
+  ],
 })
 export class AppModule {}
